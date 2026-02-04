73aa4c043c317ea2c1928409265a0b93cf396c98
Nenerox <andres.ps0308@gmail.com>
2026-02-04 12:58:45 -0600

    Selection Sort finished

--- Sorter.java
+++ Sorter.java
@@ -2,4 +2,34 @@ public class Sorter<T extends comparable<T>> {
     public T[] GnomeSort(T[] input) {
         return input;
     }
+
+    public T[] MergeSort(T[] input) {
+        return input;
+    }
+
+    public T[] QuickSort(T[] input) {
+        return input;
+    }
+
+    public T[] countSort(T[] input) {
+        return input;
+    }
+
+    public T[] SeleccionSort(T[] input) {
+        for (int i = 0; i < input.length; i++) {
+            int min_id = i;
+            
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j].compareTo(input[min_id]) < -1) {
+                    min_id = j;
+                }
+            }
+
+            T aux = input[i];
+            input[i] = input[min_id];
+            input[min_id] = aux;
+        }
+        return input;  
+    }
+
 }
