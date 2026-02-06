46e24b222920aea7c3dfd330c6785531c4db78f3
Nenerox <andres.ps0308@gmail.com>
2026-02-05 20:12:41 -0600

    Added test for Radix sort

--- Main.java
+++ Main.java
@@ -4,11 +4,16 @@ public class Main {
     public static void main(String[] args) {
         txtManager txtManager = new txtManager();
         Random random = new Random();
-        
+        Sorter<Integer> sorter = new Sorter<>();
+
         int cantidadNumeros = random.nextInt(2991) + 10; // 2991 = 3000 - 10 + 1
         
         String nombreArchivo = "Unsorted.txt";
         
         txtManager.generarNumeros(cantidadNumeros, nombreArchivo, random);
+        Integer[] arrayToSort = txtManager.getArray("Prueba.txt");
+        
+        txtManager.writeResult("Sorted.txt", sorter.radixSort(arrayToSort));
+        
     }
 }
