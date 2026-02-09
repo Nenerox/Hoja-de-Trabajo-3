9bfa290f59278a6041c087c0313386d83d69d85f
Nenerox <andres.ps0308@gmail.com>
2026-02-08 22:28:03 -0600

    Finished main

--- Main.java
+++ Main.java
@@ -7,13 +7,43 @@ public class Main {
         Sorter<Integer> sorter = new Sorter<>();
 
         int cantidadNumeros = random.nextInt(2991) + 10; // 2991 = 3000 - 10 + 1
-        
-        String nombreArchivo = "Unsorted.txt";
-        
-        txtManager.generarNumeros(cantidadNumeros, nombreArchivo, random);
-        Integer[] arrayToSort = txtManager.getArray("Prueba.txt");
-        
-        txtManager.writeResult("Sorted.txt", sorter.QuickSort(arrayToSort));
-        
+        txtManager.generarNumeros(cantidadNumeros, "Unsorted.txt", random);
+
+        System.out.println("Cantidad de numeros a sortear: " + cantidadNumeros);
+         
+        Integer[] arrayToSort = txtManager.getArray("Unsorted.txt");
+
+        //GnomeSort TEST unsorted and sorted
+        System.out.println("Sorteando con GnomeSort");
+        Integer[] SortedArray = sorter.GnomeSort(arrayToSort);
+        txtManager.writeResult("Sorted.txt", SortedArray);
+        sorter.GnomeSort(SortedArray);
+
+        //MergeSort TEST unsorted and sorted
+        System.out.println("Sorteando con MergeSort");
+        SortedArray = sorter.MergeSort(arrayToSort);
+        txtManager.writeResult("Sorted.txt", SortedArray);
+        sorter.MergeSort(SortedArray);
+
+        //QuickSort TEST unsorted and sorted
+        System.out.println("Sorteando con QuickSort");
+        SortedArray = sorter.QuickSort(arrayToSort);
+        txtManager.writeResult("Sorted.txt", SortedArray);
+        sorter.QuickSort(SortedArray);
+
+        //RadixSort TEST unsorted and sorted
+        System.out.println("Sorteando con RadixSort");
+        SortedArray = sorter.radixSort(arrayToSort);
+        txtManager.writeResult("Sorted.txt", SortedArray);
+        sorter.radixSort(SortedArray);
+
+        //SeleccionSort TEST unsorted and sorted
+        System.out.println("Sorteando con SeleccionSort");
+        SortedArray = sorter.SeleccionSort(arrayToSort);
+        txtManager.writeResult("Sorted.txt", SortedArray);
+        sorter.SeleccionSort(SortedArray);
+
+        System.out.println("Sorteo Terminado");
+
     }
 }
